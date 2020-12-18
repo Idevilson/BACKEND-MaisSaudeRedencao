b7e5522248f21c50483c90899cc1d9b6be42ce7a
Idevilson Junior <idevilsonjunior@outlook.com>
2020-12-18 14:27:38 -0300

    First commit to git hub by id (all files)

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,46 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IUserRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUserRepository,
+
+    @inject('HashProvider')
+    private hashprovider: IHashProvider,
+  ) {}
+
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Email addres alread used.');
+    }
+    console.log(name)
+    console.log(email)
+    console.log(password)
+    const hashedPassword = await this.hashprovider.generateHash(password);
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
